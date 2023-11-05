d8eb5bbbcfb17d9903601835da5cef9f8e0d2db1
Habib Shahzad <60187677+Habib-Shahzad@users.noreply.github.com>
2023-11-06 03:35:26 +0500

    Improve Image Upload Handling 🧨🤩👾

--- lib/presentation/screens/manageOrder/widgets/image_grid.dart
+++ lib/presentation/screens/manageOrder/widgets/image_grid.dart
@@ -3,65 +3,74 @@ import 'package:flutter/material.dart';
 import 'package:washpro/data/models/api/order_image/model.dart';
 import 'package:washpro/presentation/widgets/custom_elevated_button.dart';
 
-class ImagesGrid extends StatelessWidget {
-  final Map<String, UploadedImage> orderImages;
+class ImageItem extends StatefulWidget {
+  final String imageID;
+  final UploadedImage image;
   final Future<void> Function(String imageID) onDelete;
-  const ImagesGrid(
-      {super.key, required this.orderImages, required this.onDelete});
+  const ImageItem(
+      {super.key,
+      required this.image,
+      required this.onDelete,
+      required this.imageID});
+
+  @override
+  ImageItemState createState() => ImageItemState();
+}
+
+class ImageItemState extends State<ImageItem> {
+  bool isLoading = true;
 
   @override
   Widget build(BuildContext context) {
-    if (orderImages.isEmpty) {
-      return const Center(child: Text('No images to display'));
+    Image? networkImage;
+    Image? localImage;
+
+    final key = widget.imageID;
+    final image = widget.image;
+
+    if (image.url != null) {
+      networkImage = Image.network(
+        image.url ?? "",
+        fit: BoxFit.cover,
+      );
+    } else if (image.file != null) {
+      localImage = Image.file(
+        File(image.file!.path),
+        fit: BoxFit.cover,
+      );
     }
 
-    final imageKeys = orderImages.keys.toList();
-
-    return GridView.builder(
-      shrinkWrap: true,
-      padding: const EdgeInsets.all(8),
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 3,
-        crossAxisSpacing: 8,
-        mainAxisSpacing: 8,
+    networkImage?.image.resolve(const ImageConfiguration()).addListener(
+      ImageStreamListener(
+        (info, call) {
+          if (mounted) {
+            setState(() {
+              isLoading = false;
+            });
+          }
+        },
       ),
-      itemCount: imageKeys.length,
-      itemBuilder: (context, index) {
-        Widget imageWidget = const Center(child: CircularProgressIndicator());
-        final key = imageKeys[index];
-        final image = orderImages[key]!;
+    );
 
-        if (image.url != null) {
-          imageWidget = Image.network(
-            image.url ?? "",
-            fit: BoxFit.cover,
-          );
-        } else if (image.file != null) {
-          imageWidget = Image.file(
-            File(image.file!.path),
-            fit: BoxFit.cover,
-          );
-        }
-
-        return GestureDetector(
-          onTap: () {
-            if (image.url == null) return;
-            _openPreviewModal(context, imageWidget, key, onDelete);
-          },
-          child: Container(
-            padding: const EdgeInsets.all(4),
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.grey),
-              borderRadius: BorderRadius.circular(8),
-            ),
-            child: imageWidget,
-          ),
-        );
+    return GestureDetector(
+      onTap: () {
+        if (networkImage == null || isLoading == true) return;
+        _openPreviewModal(context, networkImage, key, widget.onDelete);
       },
+      child: Container(
+        padding: const EdgeInsets.all(4),
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: isLoading == true
+            ? const Center(child: CircularProgressIndicator())
+            : networkImage ?? localImage,
+      ),
     );
   }
 
-  void _openPreviewModal(BuildContext context, Widget imageWidget,
+  void _openPreviewModal(BuildContext context, Image imageWidget,
       String imageID, Future<void> Function(String imageID) onDelete) {
     showDialog(
       context: context,
@@ -98,6 +107,42 @@ class ImagesGrid extends StatelessWidget {
               ],
             ),
           ),
+        );
+      },
+    );
+  }
+}
+
+class ImagesGrid extends StatelessWidget {
+  final Map<String, UploadedImage> orderImages;
+  final Future<void> Function(String imageID) onDelete;
+  const ImagesGrid(
+      {super.key, required this.orderImages, required this.onDelete});
+
+  @override
+  Widget build(BuildContext context) {
+    if (orderImages.isEmpty) {
+      return const Center(child: Text('No images to display'));
+    }
+
+    final imageKeys = orderImages.keys.toList();
+
+    return GridView.builder(
+      shrinkWrap: true,
+      padding: const EdgeInsets.all(8),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
+        crossAxisSpacing: 8,
+        mainAxisSpacing: 8,
+      ),
+      itemCount: imageKeys.length,
+      itemBuilder: (context, index) {
+        final imageID = imageKeys[index];
+        final image = orderImages[imageID]!;
+        return ImageItem(
+          imageID: imageID,
+          image: image,
+          onDelete: onDelete,
         );
       },
     );
